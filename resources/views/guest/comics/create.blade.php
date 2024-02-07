6378ca300630947dbb9761e0971603442c7acf42
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-02-07 09:01:04 +0100

    Added title to create.blade

--- resources/views/guest/comics/create.blade.php
+++ resources/views/guest/comics/create.blade.php
@@ -3,37 +3,38 @@
 @section('main-content')
 <section class="container">
     <div class="row">
-        <div class="col8 d-flex justify-content-center">
-            <form action="">
-                <div class="mb-2">
-                    <label for="title">Title:</label>
+        <div class="d-flex justify-content-center col-12">
+            <h1>Add new Comic:</h1>
+            <form  action="">
+                <div class="mb-4 d-flex justify-content-between" >
+                    <label for="title" class="form-label">Title:</label>
                     <input type="text" name="title" id="title">
                 </div>
-                <div class="mb-2">
-                    <label for="description">Description:</label>
+                <div class="mb-4 d-flex justify-content-between">
+                    <label for="description" class="form-label">Description:</label>
                     <input type="text" name="description" id="description">
                 </div>
-                <div class="mb-2">
-                    <label for="thumb">Image url:</label>
+                <div class="mb-4 d-flex justify-content-between">
+                    <label for="thumb" class="form-label">Image url:</label>
                     <input type="text" name="thumb" id="thumb">
                 </div>
-                <div class="mb-2">
-                    <label for="price">Price:</label>
+                <div class="mb-4 d-flex justify-content-between">
+                    <label for="price" class="form-label">Price:</label>
                     <input type="text" name="price" id="price">
                 </div>
-                <div class="mb-2">
-                    <label for="series">Series:</label>
+                <div class="mb-4 d-flex justify-content-between">
+                    <label for="series" class="form-label">Series:</label>
                     <input type="text" name="series" id="series">
                 </div>
-                <div class="mb-2">
-                    <label for="sale_date">Sale date:</label>
+                <div class="mb-4 d-flex justify-content-between">
+                    <label for="sale_date" class="form-label">Sale date:</label>
                     <input type="text" name="sale_date" id="sale_date">
                 </div>
-                <div class="mb-2">
-                    <label for="type">Type:</label>
+                <div class="mb-4 d-flex justify-content-between">
+                    <label for="type" class="form-label">Type:</label>
                     <input type="text" name="type" id="type">
                 </div>
-                <button class="btn btn-success">Submit</button>
+                <button class="mb-4 btn btn-success">Submit</button>
             </form>
         </div>
     </div>
